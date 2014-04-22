93aee2b2a00c1c1e3c5a1118561f417a58ebf71a
Anand B Pillai <abp@tingtun.no>
2014-04-22 11:15:28 +0530

    Return the crawl graph after crawl end

--- eiii_crawler_client.py
+++ eiii_crawler_client.py
@@ -8,7 +8,13 @@ crawler_rules = {'max-pages': [(['text/html', 'application/xhtml+xml', 'applicat
                  'size-limits': [(['text/html', 'application/xhtml+xml', 'application/xml'], 500)],
                  'seeds': ['http://www.tingtun.no'], 'obey-robotstxt': 'false'}
 
-proxy = TTRPCProxy('tcp://localhost:8910')
-proxy.crawl(crawler_rules)
+# Wait for 3 hours for crawl to end.
+proxy = TTRPCProxy('tcp://localhost:8910', retries=1,
+                   timeout=10800*1000)
+
+# print dir(proxy.poller)
+crawl_graph = proxy.crawl(crawler_rules)
+print 'Crawl graph=>',crawl_graph
+
 # print proxy.load()
 
