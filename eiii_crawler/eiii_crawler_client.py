206bfd88bd018193f081f8d09cc3046451a65415
Anand B Pillai <abp@tingtun.no>
2014-08-04 13:40:12 +0530

    Fixing state issues across crawls (related to #398)

--- eiii_crawler/eiii_crawler_client.py
+++ eiii_crawler/eiii_crawler_client.py
@@ -3,7 +3,7 @@ the crawler server """
 
 from ttrpc.client import *
 
-crawler_rules = {'max-pages': [(['text/html', 'application/xhtml+xml', 'application/xml'], 50)],
+crawler_rules = {'max-pages': [(['text/html', 'application/xhtml+xml', 'application/xml'], 10)],
                  'scoping-rules': [('+', '^https?://utt\\.tingtun\\.no')], 'min-crawl-delay': 2,
                  'size-limits': [(['text/html', 'application/xhtml+xml', 'application/xml'], 500)],
                  'seeds': ['https://docs.python.org/2/library/'], 'obey-robotstxt': False,
@@ -16,8 +16,8 @@ proxy = TTRPCProxy('tcp://localhost:8910', retries=1,
 # print dir(proxy.poller)
 crawl_graph = proxy.crawl(crawler_rules)
 print 'Crawl graph=>',crawl_graph['result']
-print
-print 'Crawl stats=>',crawl_graph['stats']
+# print
+# print 'Crawl stats=>',crawl_graph['stats']
 
 # print proxy.load()
 
