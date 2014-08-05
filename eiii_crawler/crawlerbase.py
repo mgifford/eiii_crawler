3cc0ae0da2127ac2c427311b80c4844c97c41919
Anand B Pillai <abp@tingtun.no>
2014-08-05 20:46:26 +0530

    Refactored core classes for thread/queue crawler implementation into other modules

--- eiii_crawler/crawlerbase.py
+++ eiii_crawler/crawlerbase.py
@@ -707,35 +707,18 @@ class CrawlerScopingRules(object):
         # default value
         return ret
 
-class CrawlerWorkerBase(threading.Thread):
+class CrawlerWorkerBase(object):
     """ Base class for EIII web crawler worker. This class does most of the
     work of crawling """
 
     def __init__(self, config):
         """ Initializer - sets configuration """
 
-        self.config = config
-        self.state = 0
-        # Prepare config
-        self.prepare_config()
-        threading.Thread.__init__(self, None, None, 'CrawlerWorker-' + uuid.uuid4().hex)
+        pass
 
     def prepare_config(self):
         """ Prepare configuration """
-
         pass
-        
-    def __getattr__(self, name):
-        """ Overloaded getattr method to allow
-        access of config variables as local attributes """
-
-        try:
-            return self.__dict__[name]
-        except KeyError:
-            try:
-                return getattr(self.config, name)
-            except AttributeError:
-                raise
         
     def parse(self, content, url):
         """ Parse web-page content and return an iterator on child URLs """
@@ -743,6 +726,7 @@ class CrawlerWorkerBase(threading.Thread):
 
     def build_url(self, child_url, parent_url):
         """ Build the complete URL for child URL using the parent URL """
+
         raise NotImplementedError
 
     def supplement_urls(self, url):
@@ -766,10 +750,9 @@ class CrawlerWorkerBase(threading.Thread):
         raise NotImplementedError
 
     def get_url_data_instance(self, url, parent_url=None):
-        """ Make an instance of the URL data class
-        which fetches the URL """
-
-        return CrawlerUrlData(url, self.config)
+        """ Make an instance of the URL data class which fetches the URL """
+    
+        raise NotImplementedError
     
     def download(self, url, parent_url=None):
         """ Download a URL and return an object which
@@ -778,12 +761,7 @@ class CrawlerWorkerBase(threading.Thread):
         and content by the 'content' attribute of the
         returned object. Returns error code if failed """
 
-        urlobj = self.get_url_data_instance(url, parent_url)
-        # Double-dispatch pattern - this is so amazingly useful!
-        # Gives you effect of mulitple inheritance with objects.
-        urlobj.download(self)
-
-        return urlobj
+        raise NotImplementedError
 
     def before_crawl(self):
         """ Actions to execute before crawl starts """
@@ -812,8 +790,9 @@ class CrawlerWorkerBase(threading.Thread):
 
     def get_state(self):
         """ Return the state """
-        
-        return self.state
+
+        pass
+
     
     def push(self, content_type, url, parent_url, key=None):
         """ Push new data back """
@@ -821,135 +800,19 @@ class CrawlerWorkerBase(threading.Thread):
         raise NotImplementedError
 
     def do_crawl(self):
-        """ Do the actual crawl. This function provides a pluggable
-        crawler workflow structure. The methods called by the crawler
-        are pluggable in the sense that sub-classes need to override
-        most of them for the actual crawl to work. However the skeleton
-        of the workflow is defined by this method.
+        """ Do the actual crawl. """
 
-        A sub-class can implement a new crawl workflow by completely
-        overriding this method though it is not suggested.
-        """
-
-        while self.work_pending() and (not self.should_stop()):
-            # State is 0 - about to get data
-            self.state = 0
-            data = self.get()
-            
-            if data==((None,None,None)):
-                log.info('No URLs to crawl.')
-                break
-
-            # Convert the data to URLs - namely child URL and parent URL and any additional data
-            content_type, url, parent_url= self.parse_queue_urls(data)
-
-            # State is 1 - got data, doing work
-            self.state = 1
-            if self.allowed(url, parent_url, content_type, download=True):
-                log.info('Downloading URL',url,'...')
-                urlobj = self.download(url, parent_url)
-                
-                # Data is obtained using the method urlobj.get_data()
-                # Headers is obtained using the method urlobj.get_headers()
-                url_data = urlobj.get_data()
-                headers = urlobj.get_headers()
-
-                # Modified URL if any - this can happen if URL is forwarded
-                # etc. Child URLs would need to be constructed against the
-                # updated URL not the old one. E.g: https://docs.python.org/library/
-                url = urlobj.get_url()
-
-                # Make another call to allowed this time with the content and headers - it
-                # is up to the child class on how to implement this - for example
-                # it can chose to implement content specific rules in another function.
-                # In this case the allowed is more applicable to child URLs - for example
-                # a META robots NOFOLLOW is parsed at this point.
-
-                if (urlobj.status) and (url_data != None) and \
-                       self.allowed(url, parent_url, url_data, content_type, headers, parse=True):
-
-                    # Can proceed further
-                    # Parse the data
-                    url, child_urls = self.parse(url_data, url)
-
-                    if self.flag_randomize_urls:
-                        random.shuffle(child_urls)
-                        
-                    newurls = []
-                    
-                    for curl in child_urls:
-                        # Skip empty strings
-                        if len(curl.strip())==0: continue
-                        # Build full URL
-                        full_curl = self.build_url(curl, url)
-                        if len(full_curl)==0: continue
-
-                        # Insert this back to the queue
-                        content_type = urlhelper.get_content_type(full_curl, headers)
-                        # Skip if not allowed
-                        if self.allowed(full_curl, parent_url=url, content_type=content_type):
-                            # log.info(url," => Adding URL",full_curl,"...")
-                            newurls.append((content_type, full_curl, url))
-
-                            # Build additional URLs if any
-                            other_urls = self.supplement_urls(full_curl)
-                            for other_url in other_urls:
-                                if self.allowed(other_url, parent_url=url, content_type='text/html'):
-                                    # Safely assume HTML for directory URLs
-                                    newurls.append(('text/html', other_url, url))                           
-                        else:
-                            log.debug('Skipping URL',full_curl,'...')
-
-                    # State is 2, did work, pushing new data
-                    self.state = 2
-                    newurls = list(set(newurls))
-                    
-                    # Push data into the queue
-                    for ctype, curl, purl in newurls:
-                        # Key is the child URL itself
-                        if self.push(ctype, curl, purl, curl):
-                            log.debug("\tPushed new URL =>",curl,"...")
-                else:
-                    if url_data == None:
-                        log.debug("URL data is null =>", url)
-                    else:
-                        log.debug("URL is disallowed =>", url)
-
-            else:
-                log.debug('Skipping URL',url,'...')
-
-            # State is 3, sleeping off
-            self.state = 3
-            self.sleep()
-
-        # Put state to zero when exiting
-        self.state = 0
-
-        log.info('Worker',self,'done.')
+        raise NotImplementedError
 
     def sleep(self):
         """ Sleep it off """
         
-        # Sleep
-        if self.flag_randomize_sleep:
-            # Randomize 50% on both sides
-            time.sleep(random.uniform(self.time_sleeptime, self.time_sleeptime*2))
-        else:
-            time.sleep(self.time_sleeptime)
+        raise NotImplementedError
 
     def after_crawl(self):
         """ Actions to execute after the crawl """
-
         pass
     
-    def run(self):
-        """ Do the actual crawl """
-
-        log.info('Worker',self,'starting...')
-        # Defines the "framework" for crawling
-        self.before_crawl()
-        self.do_crawl()
-        self.after_crawl()
     
 if __name__ == "__main__":
     CrawlerConfig().save_default()
