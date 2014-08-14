ad6676e93e59160e45800ffc8f71ddd35097436a
Anand B Pillai <abp@tingtun.no>
2014-08-14 18:42:51 +0530

    Multi-processing, Load balancing, et cetera, et cetera

--- eiii_crawler/eiii_crawler_client.py
+++ eiii_crawler/eiii_crawler_client.py
@@ -2,22 +2,27 @@
 the crawler server """
 
 from ttrpc.client import *
+import sys
 
-crawler_rules = {'max-pages': [(['text/html', 'application/xhtml+xml', 'application/xml'], 10)],
-                 'scoping-rules': [('+', '^https?://utt\\.tingtun\\.no')], 'min-crawl-delay': 2,
+crawler_rules = {'max-pages': [(['text/html', 'application/xhtml+xml', 'application/xml'], 50)],
+                 'scoping-rules': [('+','^anand.*')], 'min-crawl-delay': 2,
                  'size-limits': [(['text/html', 'application/xhtml+xml', 'application/xml'], 500)],
-                 'seeds': ['https://docs.python.org/2/library/'], 'obey-robotstxt': False,
+                 'seeds': ['https://mada.org.qa'], 'obey-robotstxt': False,
                  'loglevel': 'debug'}
 
 # Wait for 3 hours for crawl to end.
 proxy = TTRPCProxy('tcp://localhost:8910', retries=1,
                    timeout=10800*1000)
 
-# print dir(proxy.poller)
-crawl_graph = proxy.crawl(crawler_rules)
-print 'Crawl graph=>',crawl_graph['result']
-# print
-# print 'Crawl stats=>',crawl_graph['stats']
+# Set seeds
+crawler_rules['seeds'] = sys.argv[1:]
+print crawler_rules['seeds']
 
-# print proxy.load()
+# print dir(proxy.poller)
+task_id = proxy.crawl(crawler_rules)
+# Wait for results.
+print 'Task_id=>',task_id
+crawl_graph = proxy.poll(task_id)
+
+print 'Crawl graph=>',crawl_graph['result']
 
