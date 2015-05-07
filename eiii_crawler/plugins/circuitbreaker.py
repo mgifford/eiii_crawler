9068a4ac318fc2f6bc6d4e1b6c8d0e5a26541f18
Anand B Pillai <abp@tingtun.no>
2015-05-07 20:19:23 +0530

    Final fixes for issue #429

--- eiii_crawler/plugins/circuitbreaker.py
+++ eiii_crawler/plugins/circuitbreaker.py
@@ -11,13 +11,34 @@ import re
 
 from eiii_crawler.crawlerevent import subscribe
 from eiii_crawler.crawlerbase import CrawlerConfig
+from eiii_crawler import utils
 
 # Dictionary mapping dynamic URL regexes to their hit counts
 __regexes__ = {}
 # Dictionary mapping dynamic URL regex to their total URLs count
 __regexurls__ = {}
 
-@subscribe('url_pushed')
+# Default logging object
+log = utils.get_default_logger()
+
+# Global configuration
+# These are in this case,
+# 1. Min # of URLs of a specific pattern to be found before threshod is imposed
+# 2. The actual threshold of percentage of such URLs over # of total URLs (from the
+# time the rule has been created). The dynamic rules are created only after the threshold
+# is hit.
+
+min_hits, threshold = 10, 20.0
+
+def set_config(**kwargs):
+    """ Set configuration for the plugin """
+
+    for key,value in kwargs.items():
+        # Set globally
+        log.info("\tSetting configuration",key,"to",value)
+        globals()[key] = value
+    
+@subscribe('download_complete')
 def check_circuit(event):
     """ Check for circuits (loops) in URL patterns """
 
@@ -32,7 +53,6 @@ def check_circuit(event):
     if query_p:
         # Check against any of the existing regexes
         # Split and normalize the query parameter
-        # 'Uid=1026&Cid=9' => 'Cid=9&Uid=1026'
         query_p_norm = '&'.join(map(lambda x:x.strip(), query_p.split('&')))
         hit = False
         
@@ -60,19 +80,21 @@ def check_circuit(event):
             # Hit - check percentage - if >=20% of total URLs seen so far, mark this as
             # a repeating template. Also do this only after at least 10 actual hits
             count, total = __regexes__[regex], __regexurls__[regex]
-            if (total>=10) and 1.0*count/total >= 0.2:
+            
+            if (total>=min_hits) and 100.0*count/total >= threshold:
                 # print 'Template hit for regex =>',regex.pattern, count, total
                 # Append this rule to exclude of crawler config
                 # Prefix a .* before the rule pattern to be a catch-all on prefix
                 rule = '.*' + regex.pattern
                 config = CrawlerConfig.getInstance()
-                if rule not in config._url_exclude_rules:
-                    print 'Creating dynamic rule to exclude',regex.pattern,'...'
-                    config._url_exclude_rules.append(rule)
+                if rule not in config._url_dynamic_exclude_rules:
+                    log.info('Hit threshold. Creating dynamic rule to exclude',regex.pattern,'...')
+                    config._url_dynamic_exclude_rules.append(rule)
                     # print '\tnew exclusion rules=>',config._url_exclude_rules
                     # Drop this rule from the dictionary
                     del __regexurls__[regex]
                     del __regexes__[regex]
             
     else:
-        print 'No query param found, not doing anything'
+        # print 'No query param found, not doing anything'
+        pass
